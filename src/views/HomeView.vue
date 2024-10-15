649987c47ea6dba48d31a276796545042b28e268
Niya <53850933+niyati-dot@users.noreply.github.com>
2024-10-15 12:22:25 -0600

    Add files via upload

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam explicabo placeat delectus illum? Quo, perspiciatis suscipit nam nisi veniam quas quibusdam. Minima atque facere minus dolorum blanditiis praesentium maiores, quidem temporibus nemo sed officiis nesciunt architecto eligendi magni reiciendis quaerat qui, numquam, perspiciatis omnis porro est voluptatibus aliquam accusantium. Soluta, nemo. Omnis, blanditiis eos? Nostrum, nam maiores. Omnis animi facere corrupti, qui tempora, eos, quos praesentium totam illum maxime quae. Consectetur provident ducimus voluptates eum. Fuga, molestiae. Fuga asperiores porro odio quidem officiis illo provident reprehenderit et aliquam. Quam inventore minima similique vero id, voluptates ea culpa soluta excepturi quaerat! </p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam explicabo placeat delectus illum? Quo, perspiciatis suscipit nam nisi veniam quas quibusdam. Minima atque facere minus dolorum blanditiis praesentium maiores, quidem temporibus nemo sed officiis nesciunt architecto eligendi magni reiciendis quaerat qui, numquam, perspiciatis omnis porro est voluptatibus aliquam accusantium. Soluta, nemo. Omnis, blanditiis eos? Nostrum, nam maiores. Omnis animi facere corrupti, qui tempora, eos, quos praesentium totam illum maxime quae. Consectetur provident ducimus voluptates eum. Fuga, molestiae. Fuga asperiores porro odio quidem officiis illo provident reprehenderit et aliquam. Quam inventore minima similique vero id, voluptates ea culpa soluta excepturi quaerat! </p>
+  </div>
+</template>
 
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+   
+  }
+}
+</script>
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>404</h2>
+  <p>Page Not Found</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
