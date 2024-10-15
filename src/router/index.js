0fd86c81d3db94ee54a777c67d83dd65257c6084
Niya <53850933+niyati-dot@users.noreply.github.com>
2024-10-15 12:21:25 -0600

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import JobsView from '../views/Jobs/JobsView.vue'
+import JobDetailsView from '../views/Jobs/JobDetailsView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: JobsView
+  },
+  {
+    path: '/job/:id',
+    name: 'jobDetails',
+    component: JobDetailsView,
+    props: true
+  },
+  {
+    path: '/all-jobs', //that's how we can redirect links
+    redirect: '/jobs'
+  },
+  // catchall 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'notFound',
+    component: NotFoundView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
